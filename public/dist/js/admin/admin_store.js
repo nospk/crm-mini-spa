87e645f6fd1571fce9d64c2b39dcf57ab00324dc
nospktran <nospk1706@gmail.com>
2020-06-27 07:49:17 +0700

    fix update

---
+++ public/dist/js/admin/admin_store.js
@@ -0,0 +1,91 @@
+
+$( document ).ready(()=>{
+	get_store();
+})
+function render_store(data){
+	let html = '';
+	data.forEach(item =>{
+		html+=`<div class="col-6">
+				<div class="card">
+				  <img class="card-img-top" src="${item.image_store}" alt="Hình cửa hàng">
+				  <div class="card-body">
+					<h5 class="card-title">${item.name}</h5>
+					<p class="card-text">${item.address}</p>
+					<a href="#" class="btn btn-primary float-right">Chọn cửa hàng</a>
+				  </div>
+				 </div>
+			   </div>`
+	})
+	$('#show_store').html(html);
+}
+function create_store(){
+    let data = {
+        name: $('#create_store #name').val().trim(),
+        address: $('#create_store #address').val().trim(),
+        _csrf: $('#_csrf').val()
+    }
+    $.ajax({
+        url:'/admin_store/create',
+        method:'POST',
+        data: data,
+        success: function(data){
+            if(data.status == 1){
+                swal({
+                    title: "Cập nhật thành công",
+                    text: "Nội dung này đã được lưu lại",
+                    type: "info",
+                    showConfirmButton: false,
+                    timer: 3000
+                }).then((result)=>{
+					get_store()
+                })
+                .catch(timer => {
+					get_store()
+                });    
+            }else{
+                Swal.fire({
+                    title: "Phát sinh lỗi",
+                    text: "Vui lòng kiểm tra dữ liệu",
+                    icon: "error",
+                    showConfirmButton: false,    
+                    timer: 3000
+                }).then((result)=>{
+                    // cho vào để ko báo lỗi uncaught
+                })
+                .catch(timer => {
+                    // cho vào để ko báo lỗi uncaught
+                }); 
+                
+            }
+        }
+    })
+}
+function get_store(){
+    let data = {
+        _csrf: $('#_csrf').val()
+    }
+    $.ajax({
+        url:'/admin_store/get',
+        method:'POST',
+        data: data,
+        success: function(data){
+            if(data.status == 1){
+                render_store(data.data);
+            }else{
+                Swal.fire({
+                    title: "Phát sinh lỗi",
+                    text: "Vui lòng kiểm tra dữ liệu",
+                    icon: "error",
+                    showConfirmButton: false,    
+                    timer: 3000
+                }).then((result)=>{
+                    // cho vào để ko báo lỗi uncaught
+                })
+                .catch(timer => {
+                    // cho vào để ko báo lỗi uncaught
+                }); 
+                
+            }
+        }
+    })
+}
---
+++ router/admin/admin_store.js
@@ -0,0 +1,8 @@
+const  admin_store = require('../../app/controllers/admin/admin_store');
+
+
+module.exports = function (app) {
+    app.get("/admin_store", admin_store.loggedInadmin, admin_store.show)// render page default
+	app.post("/admin_store/create", admin_store.loggedInadmin, admin_store.create_store)
+	app.post("/admin_store/get", admin_store.loggedInadmin, admin_store.get_store)
+}

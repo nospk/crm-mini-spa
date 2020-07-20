8e832599bdb9ff8779f67c8b3b5fd6f8b222962b
nospktran <nospk1706@gmail.com>
2020-07-20 16:48:31 +0700

    update live stocks

--- public/dist/js/admin/admin_stocks.js
+++ public/dist/js/admin/admin_stocks.js
@@ -29,10 +29,21 @@ function add_product(){
     let html = `<tr>
                     <td>${value[0]}</td>
                     <td>${value[1]}</td>
-                    <td><input class="form-control form-control-sm" type="number" value="${value[2]}"></td>
-                    <td><input class="form-control form-control-sm" type="number" value="1"></td>
-                    <td>${value[2]}</td>
+                    <td><input class="form-control form-control-sm" id="cost-price-${value[1]}" onchange="change_cost_price('${value[1]}')" field="${value[1]}" type="number" step="1000" value="${value[2]}"></td>
+                    <td><input class="form-control form-control-sm" min="0" type="number" onchange="change_stocks('${value[1]}')" id="stocks-${value[1]}" field="${value[1]}" value="1"></td>
+                    <td><span id="total-${value[1]}" field="${value[1]}">${value[2]}</span></td>
                     <td><span style="color:red; cursor: pointer"><i class="fas fa-times-circle"></i></span></td>
                 </tr>`
     $('#add_product').append(html)
+}
+
+function change_cost_price(code){
+	let value = $(`#cost-price-${code}`).val()
+	let number = $(`#stocks-${code}`).val()
+	$(`#total-${code}`).text(value*number)
+}
+function change_stocks(code){
+	let value = $(`#cost-price-${code}`).val()
+	let number = $(`#stocks-${code}`).val()
+	$(`#total-${code}`).text(value*number)
 }

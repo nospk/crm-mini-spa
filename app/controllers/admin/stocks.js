cdc29229c8cad71f88431d61efed32ff02401548
nospk <nospk1706@gmail.com>
2020-07-21 20:52:46 +0700

    update socks

--- app/controllers/admin/stocks.js
+++ app/controllers/admin/stocks.js
@@ -17,6 +17,28 @@ class Admin_stocks extends Controller{
 			Admin_stocks.sendError(res, err, err.message);
         }
     }
+    static async create_new(req, res){
+		try{
+            let products = req.body.products;
+            for (let i = 1; i < products.length; i++){
+                let find = await Product_service.findOne({admin_id: req.session.user._id, type: "product", number_code: products[i].number_code})
+                let cost_price_average = ((Number(find.stocks) * Number(find.cost_price)) + (Number(products[i].stock_amount) * Number(products[i].cost_price))) / (Number(find.stocks) + Number(products[i].stock_amount))
+                find.stocks = Number(find.stocks) + Number(products[i].stock_amount)
+                find.cost_price = cost_price_average
+                for (let j = 0; j < find.stocks_in.length; j ++){
+                    if(find.stocks_in[j].store == req.session.store_id){
+                        find.stocks_in[j].stock_amount = Number(find.stocks_in[j].stock_amount) + Number(products[i].stock_amount)
+                    }
+                }
+                await find.save()
+            }
+            Admin_stocks.sendMessage(res, "Đã tạo thành công");
+		}catch(err){
+			console.log(err)
+			Admin_stocks.sendError(res, err, err.message);
+		}
+		
+	}
 }
 
 module.exports = Admin_stocks
--- public/dist/js/admin/admin_stocks.js
+++ public/dist/js/admin/admin_stocks.js
@@ -1,3 +1,4 @@
+let money_total;
 $( document ).ready(()=>{
 	get_product();
 })
@@ -26,24 +27,122 @@ function get_product(){
 function add_product(){
     let value = $('#select_product').val()
     value = value.split(':')
-    let html = `<tr>
-                    <td>${value[0]}</td>
-                    <td>${value[1]}</td>
-                    <td><input class="form-control form-control-sm" id="cost-price-${value[1]}" onchange="change_cost_price('${value[1]}')" field="${value[1]}" type="number" step="1000" value="${value[2]}"></td>
-                    <td><input class="form-control form-control-sm" min="0" type="number" onchange="change_stocks('${value[1]}')" id="stocks-${value[1]}" field="${value[1]}" value="1"></td>
-                    <td><span id="total-${value[1]}" field="${value[1]}">${value[2]}</span></td>
-                    <td><span style="color:red; cursor: pointer"><i class="fas fa-times-circle"></i></span></td>
+    let check = $(`#stocks-${value[1]}`).val()
+    if(!check){
+        let html = `<tr>
+                    <td><span>${value[0]}</span></td>
+                    <td><span class="number-code">${value[1]}</span></td>
+                    <td><input class="form-control form-control-sm" id="cost-price-${value[1]}" onchange="change_cost_price('${value[1]}')" type="number" step="1000" value="${value[2]}"></td>
+                    <td><input class="form-control form-control-sm" min="0" type="number" onchange="change_stocks('${value[1]}')" id="stocks-${value[1]}" value="1"></td>
+                    <td><span class="total" id="total-${value[1]}" >${value[2]}</span></td>
+                    <td><span style="color:red; cursor: pointer" onclick="delete_row_product(this)"><i class="fas fa-times-circle"></i></span></td>
                 </tr>`
-    $('#add_product').append(html)
+        $('#add_product').append(html)
+        total_get_goods();
+    }else{
+        let amount = $(`#stocks-${value[1]}`).val()
+        $(`#stocks-${value[1]}`).val(Number(amount)+1)
+        change_stocks(value[1])
+    }
 }
-
+function delete_row_product(btn) {
+    var row = btn.parentNode.parentNode;
+    row.parentNode.removeChild(row);
+    total_get_goods();
+}
 function change_cost_price(code){
 	let value = $(`#cost-price-${code}`).val()
 	let number = $(`#stocks-${code}`).val()
-	$(`#total-${code}`).text(value*number)
+    $(`#total-${code}`).text(value*number)
+    total_get_goods();
 }
 function change_stocks(code){
 	let value = $(`#cost-price-${code}`).val()
 	let number = $(`#stocks-${code}`).val()
-	$(`#total-${code}`).text(value*number)
+    $(`#total-${code}`).text(value*number)
+    total_get_goods();
+}
+function total_get_goods(){
+    let money = 0;
+    $(".total").each(function () {                  
+        money+= Number($(this).text()); 
+    });
+    $('#total_get_goods').text(new Intl.NumberFormat().format(money))
+    money_total = money;
+}
+
+function get_list_product(){
+    let list_product = [];
+    $(".number-code").each(function () {                  
+        list_product.push($(this).text()); 
+    });
+    let data = [{
+        total_get_goods : money_total
+    }];
+    list_product.forEach((number_code)=>{
+        data.push({
+            number_code : number_code,
+            cost_price: $(`#cost-price-${number_code}`).val(),
+            stock_amount: $(`#stocks-${number_code}`).val(),
+        })
+    })
+    return data;
+}
+
+function create_new(){
+    let data = {
+        products: get_list_product(),
+        _csrf: $('#_csrf').val()
+    }
+    if(data.products.length > 1){
+        $.ajax({
+            url:'/admin_stocks/create',
+            method:'POST',
+            data: data,
+            success: function(data){
+                if(data.status == 1){
+                    Swal.fire({
+                        title: "Thao tác thành công",
+                        text: data.message,
+                        icon: "info",
+                        showConfirmButton: false,
+                        timer: 3000
+                    }).then((result)=>{
+                        get_product()
+                    })
+                    .catch(timer => {
+                        get_product()
+                    });    
+                }else{
+                    Swal.fire({
+                        title: data.error,
+                        text: data.message,
+                        icon: "error",
+                        showConfirmButton: false,    
+                        timer: 3000
+                    }).then((result)=>{
+                        // cho vào để ko báo lỗi uncaught
+                    })
+                    .catch(timer => {
+                        // cho vào để ko báo lỗi uncaught
+                    }); 
+                    
+                }
+            }
+        })
+    }else{
+        Swal.fire({
+            title: 'Lỗi thiếu thông tin hàng nhập',
+            text: 'Vui lòng nhập hàng hóa vào',
+            icon: "error",
+            showConfirmButton: false,    
+            timer: 3000
+        }).then((result)=>{
+            // cho vào để ko báo lỗi uncaught
+        })
+        .catch(timer => {
+            // cho vào để ko báo lỗi uncaught
+        }); 
+    }
+    
 }

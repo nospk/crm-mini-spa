6f56498d0a79fc842f19f07f86e2f638c4dbaebe
nospk <nospk1706@gmail.com>
2021-01-03 12:44:37 +0700

    update

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const express =  require('express');
+const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const fs = require('fs');
@@ -19,7 +19,7 @@ const options = {
     useNewUrlParser: true,
     useCreateIndex: true,
     autoIndex: true,
-    useFindAndModify: false, 
+    useFindAndModify: false,
     useUnifiedTopology: true
 }
 /*************************** Main *******************************/
@@ -27,8 +27,8 @@ const options = {
 
 //helmet protect express
 app.use(helmet())
-app.use(session({ 
-    secret: 'this-is-a-secret-token-a-b-2-3-4', 
+app.use(session({
+    secret: 'this-is-a-secret-token-a-b-2-3-4',
     cookie: { maxAge: 28800000 },
     resave: false,
     saveUninitialized: true,
@@ -36,13 +36,13 @@ app.use(session({
 }));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //morgan log access - except type GET
 // app.use(morgan('common', {
-    // skip: function (req, res) { 
-        // return req.method == 'GET' 
-    // }
+// skip: function (req, res) { 
+// return req.method == 'GET' 
+// }
 // }))
 require('./core/passport')(passport); // pass passport for configuration
 app.use(passport.initialize());
@@ -57,7 +57,7 @@ app.set('views', path.join('./app/views'));
 app.set('view engine', 'ejs');
 
 // connect to our database 
-mongoose.connect(process.env.DB_MONGO, options);  
+mongoose.connect(process.env.DB_MONGO, options);
 //mongoose.connect(process.env.DB_LOCALHOST, options);  
 
 app.use(cookieParser())
@@ -70,32 +70,43 @@ app.use(function (req, res, next) {
 });
 
 //add one folder then put your route files there my router folder name is routers
-const routeBasePath="./router/"; 
-fs.readdirSync(routeBasePath).forEach(function(file) {
-    let route=routeBasePath+file;
-	if(fs.lstatSync(route).isFile()){
-		if(file == 'login.js' || file == 'signup.js'){
-			require(route)(app, passport);
-		}else{
-			require(route)(app);
-		}
-	}
+const routeBasePath = "./router/";
+fs.readdirSync(routeBasePath).forEach(function (file) {
+    let route = routeBasePath + file;
+    if (fs.lstatSync(route).isFile()) {
+        if (file == 'login.js' || file == 'signup.js') {
+            require(route)(app, passport);
+        } else {
+            require(route)(app);
+        }
+    }
 });
-const routeAdminManagerPath="./router/admin_manager/";
-fs.readdirSync(routeAdminManagerPath).forEach(function(file) {
-    let route=routeAdminManagerPath+file;
-    require(route)(app);
-});
-const routeAdminStorePath="./router/admin_store/";
-fs.readdirSync(routeAdminStorePath).forEach(function(file) {
-    let route=routeAdminStorePath+file;
-    require(route)(app);
-});
-const routeStorePath="./router/store/";
-fs.readdirSync(routeStorePath).forEach(function(file) {
-    let route=routeStorePath+file;
-    require(route)(app);
-});
+if (process.env.NODE_ENV === 'production') {
+    // We are running in production mode
+    const routeStorePath = "./router/store/";
+    fs.readdirSync(routeStorePath).forEach(function (file) {
+        let route = routeStorePath + file;
+        require(route)(app);
+    });
+} else {
+    // We are running in development mode
+    const routeAdminManagerPath = "./router/admin_manager/";
+    fs.readdirSync(routeAdminManagerPath).forEach(function (file) {
+        let route = routeAdminManagerPath + file;
+        require(route)(app);
+    });
+    const routeAdminStorePath = "./router/admin_store/";
+    fs.readdirSync(routeAdminStorePath).forEach(function (file) {
+        let route = routeAdminStorePath + file;
+        require(route)(app);
+    });
+    const routeStorePath = "./router/store/";
+    fs.readdirSync(routeStorePath).forEach(function (file) {
+        let route = routeStorePath + file;
+        require(route)(app);
+    });
+}
+
 
 //Catch not found
 app.use(error_handler.notFound);

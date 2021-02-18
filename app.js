78833c9c45fd94eb2e16a79e238245c1091592ba
nospktran <nospk1706@gmail.com>
2021-02-18 08:01:49 +0700

    update set headers

--- app.js
+++ app.js
@@ -86,13 +86,14 @@ if (process.env.NODE_ENV === 'production') {
 app.use(function (req, res, next) {
     let token = req.csrfToken();
 	var allowedOrigins = ['https://app.nospk.dev'];
-	var origin = req.headers.origin;
+	var origin = req.headers.host;
+	console.log(origin);
     if(allowedOrigins.indexOf(origin) > -1){
-       res.setHeader('Access-Control-Allow-Origin', origin);
+       res.set('Access-Control-Allow-Origin', origin);
     }
-    res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Allow-Credentials', true);
+    res.set('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.set('Access-Control-Allow-Credentials', true);
     res.locals.csrfToken = token;
     next();
 });

537dfda6ddc3aa674d047b196377c5d3d66f9290
nospk <nospk1706@gmail.com>
2022-01-01 13:37:03 +0700

    fix report

--- app/controllers/admin_store/store_report.js
+++ app/controllers/admin_store/store_report.js
@@ -13,9 +13,9 @@ class Admin_store_report extends Controller {
         try {
             let now = new Date();
             let start_month = new Date(now.getFullYear(), now.getMonth(), 1, 0, 0, 0);
-            let end_month = new Date(now.getFullYear(), Number(now.getMonth()) + 1, 0, 0, 0, 0);
+            let end_month = new Date(now.getFullYear(), Number(now.getMonth()) + 1, 1, 0, 0, 0);
             let start_month_ago = new Date(now.getFullYear(), now.getMonth() -1, 1, 0, 0, 0);
-            let end_month_ago = new Date(now.getFullYear(), Number(now.getMonth()), 0, 0, 0, 0);
+            let end_month_ago = new Date(now.getFullYear(), Number(now.getMonth()), 1, 0, 0, 0);
             let gettotalAmount = await Invoice_sell.aggregate([
                 { $match: { company: mongoose.Types.ObjectId(req.session.user.company._id), isActive: true, store: mongoose.Types.ObjectId(req.session.store_id), createdAt: { $gte: start_month, $lt: end_month } } },
                 {
@@ -58,7 +58,7 @@ class Admin_store_report extends Controller {
             gettotalCostPrice = gettotalCostPrice.length > 0 ? gettotalCostPrice[0].money : 0;
             gettotalAmountLastMonth = gettotalAmountLastMonth.length > 0 ? gettotalAmountLastMonth[0].money : 0;
             gettotalCostPriceLastMonth = gettotalCostPriceLastMonth.length > 0 ? gettotalCostPriceLastMonth[0].money : 0;
-            Admin_store_report.sendData(res, { gettotalAmount, gettotalCostPrice, gettotalAmountLastMonth, gettotalCostPriceLastMonth });
+            Admin_store_report.sendData(res, { gettotalAmount, gettotalCostPrice, gettotalAmountLastMonth, gettotalCostPriceLastMonth});
         } catch (err) {
             console.log(err.message)
             Admin_store_report.sendError(res, err, err.message);
